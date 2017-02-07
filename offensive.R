4b2427a3581ddbc41e87b6b7dafd83f953ae7a8f
Matt Craddock <monklefish@gmail.com>
2017-02-07 23:13:25 +0000

    Just some more updates to the offensive words

--- offensive.R
+++ offensive.R
@@ -1,1 +1,1 @@
-bannedWords <- c("hang(ed)","murder","kill(er|ing|ed|)?","dea(th|d|dly)","genocide","injur(e|i?es|ed|y)","die(s|d)","cancer")
+bannedWords <- c("assault(ing|ed)","hang(ed)","murder","kill(er|ing|ed|)","dea(th|d|dly)","genocide","injur(e|i?es|ed|y)","die(s|d)","cancer","rap(e|ed|ist)s?","racis(t|m)s?","stab(bed|ing)?s?")
