e434dcb40b5b402fa8090d5de30c3d9948f3339c
Matt Craddock <monklefish@gmail.com>
2017-03-08 13:05:26 +0000

    minor updates

--- offensive.R
+++ offensive.R
@@ -1,1 +1,1 @@
-bannedWords <- c("assault(ing|ed)","hang(ed)","murder","kill(er|ing|ed|)","dea(th|d|dly)","genocide","injur(e|i?es|ed|y)","die(s|d)","cancer","rap(e|ed|ist)s?","racis(t|m)s?","stab(bed|ing)?s?")
+bannedWords <- c("assault(ing|ed)","hang(ed)","murder","kill(ing|ed|er|s)?s?","dea(th|d|dly)","genocide","injur(e|i?es|ed|y)","die(s|d)","cancer","rap(e|ed|ist|ing)?s?","racis(t|m)s?","stab(bed|ing)?s?")
